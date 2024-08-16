71939abe2a6edb5c2f85f03ac28a89e7ffc4be90
Sadia Rais <sadiarais222@gmail.com>
2024-08-16 10:26:20 +0600

    1st simple practice project commit

---
+++ countValue/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  let addValue = () => {
+    if (count < 20) {
+      setCount(count + 1);
+    }
+  };
+
+  let removeValue = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  let resetValue = () => {
+    setCount(0);
+  };
+
+  return (
+    <>
+      <button className="button" onClick={addValue}>
+        Add value
+      </button>
+
+      <button className="button" onClick={removeValue}>
+        Remove value
+      </button>
+      <h2>count value:{count}</h2>
+      <button className="button" onClick={resetValue}>
+        Reset value
+      </button>
+    </>
+  );
+}
+
+export default App;
